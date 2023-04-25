fd750693cb7523895bb1087469ea03386eff9f49
Jordan Lee Osier <jordan.osier@skillmaple.com>
2023-04-24 21:52:57 -0400

    Reconfirm if someone tries to sign in without verifying their email

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module pre-user-authentication-handler
 
 go 1.20
 
-require github.com/aws/aws-lambda-go v1.40.0 // indirect
+require (
+	github.com/aws/aws-lambda-go v1.40.0 // indirect
+	github.com/aws/aws-sdk-go v1.44.249 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -5,10 +5,38 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
 )
 
 func handler(event events.CognitoEventUserPoolsPreAuthentication) (events.CognitoEventUserPoolsPreAuthentication, error) {
 	fmt.Printf("PreAuthentication of user: %s\n", event.UserName)
+
+	if event.Request.UserAttributes["email_verified"] == "false" {
+
+		awsSession := session.Must(session.NewSessionWithOptions(session.Options{
+			SharedConfigState: session.SharedConfigEnable,
+		}))
+
+		cognitoClient := cognitoidentityprovider.New(awsSession)
+
+		params := &cognitoidentityprovider.ResendConfirmationCodeInput{
+			ClientId: aws.String(event.CallerContext.ClientID),
+			Username: aws.String(event.UserName),
+		}
+
+		_, err := cognitoClient.ResendConfirmationCode(params)
+		if err != nil {
+			fmt.Println("Error resending the verification email: ", err)
+			return event, fmt.Errorf("Error resending the verification email: %v", err)
+		}
+		fmt.Println("Verification email resent. Please check your email and verify your account before signing in.")
+		return event, fmt.Errorf("Verification email resent. Please check your email and verify your account before signing in.")
+	} else {
+		fmt.Println("User email already verified. Sign in successful.")
+	}
+
 	return event, nil
 }
 
